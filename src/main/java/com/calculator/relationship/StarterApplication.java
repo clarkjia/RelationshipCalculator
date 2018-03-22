c899dc2c1ddccc61da1c2af6056e4904a516601f
clarkjia <jiaaq@126.com>
2018-03-22 16:25:36 +0800

    修改为war配置

--- src/main/java/com/calculator/relationship/StarterApplication.java
+++ src/main/java/com/calculator/relationship/StarterApplication.java
@@ -2,13 +2,27 @@ package com.calculator.relationship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @ComponentScan("com.calculator.relationship")
 @SpringBootApplication
-public class StarterApplication {
+public class StarterApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(StarterApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(StarterApplication.class, args);
-	}
+    @RequestMapping("/appState")
+    String appState() {
+        return "OK";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApplication.class, args);
+    }
+
 }
